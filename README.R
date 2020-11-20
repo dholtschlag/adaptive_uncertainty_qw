7f3d4f2740576d3256fa0b5cfae111a88b1b9880
Dave Holtschlag <dholtschlag@gmail.com>
2020-11-20 13:33:01 -0500

    Commit readme file.

---
+++ README.R
@@ -0,0 +1,3 @@
+# The Adaptive Uncertainty in Water Quality project provides a basis for
+# temporally adapting model parameters, estimating missing values for surrogate
+# values, and expressing the uncertainty of measured concentratiions.
